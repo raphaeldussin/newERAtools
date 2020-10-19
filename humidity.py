7eab33fcd757993591cc2003b428c7f5dd285d5e
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-10-19 19:16:18 -0400

    fix+parser

--- humidity.py
+++ humidity.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
 import numpy as np
 import xarray as xr
 from numba import vectorize
+import argparse
 
 
 def q2_from_d2_and_msl(d2, msl):
@@ -12,7 +15,8 @@ def q2_from_d2_and_msl(d2, msl):
 
     """
 
-    q2 = xr.apply_ufunc(q2_from_d2_and_msl_raw, d2, msl)
+    q2 = xr.apply_ufunc(q2_from_d2_and_msl_raw, d2, msl, dask='parallelized',
+                        output_dtypes=[d2.dtype])
 
     return q2
 
@@ -53,3 +57,60 @@ def q2_from_d2_and_msl_raw(d2, msl):
     q2 = reps * psat / (msl - (1.0 - reps) * psat)
 
     return q2
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="compute specific humidity file from d2 and msl"
+    )
+    parser.add_argument(
+        "--d2",
+        type=str,
+        required=True,
+        help="name of dewpoint file",
+    )
+    parser.add_argument(
+        "--msl",
+        type=str,
+        required=True,
+        help="name of pressure file",
+    )
+    parser.add_argument(
+        "--d2var",
+        type=str,
+        required=False,
+        default='d2m',
+        help="name of dewpoint variable",
+    )
+    parser.add_argument(
+        "--mslvar",
+        type=str,
+        required=False,
+        default='msl',
+        help="name of pressure variable",
+    )
+    parser.add_argument(
+        "-o",
+        "--fileout",
+        type=str,
+        required=True,
+        help="name of produced specific humidity file",
+    )
+    parser.add_argument(
+        "-c",
+        "--chunks",
+        type=int,
+        required=False,
+        default=10,
+        help="size of chunk in time",
+    )
+
+    args = vars(parser.parse_args())
+
+    d2 = xr.open_dataset(args["d2"], chunks={'time': args["chunks"]})[args["d2var"]]
+    msl = xr.open_dataset(args["msl"], chunks={'time': args["chunks"]})[args["mslvar"]]
+
+    q2 = q2_from_d2_and_msl(d2, msl)
+
+    q2.to_dataset(name='q2').to_netcdf(args["fileout"])
